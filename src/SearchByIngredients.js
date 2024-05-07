3f9f1f68b07d659d4cca860a4d781a88db6dd760
Brenda Cheptoo <bcheptoo85@gmail.com>
2024-05-07 20:23:51 +0300

    SearchByIngedients component Created

--- src/SearchByIngredients.js
+++ src/SearchByIngredients.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SearchByIngredients(){
+    const[ ingredients, setIngredients]=useState("")
 }
 
 export default SearchByIngredients;
