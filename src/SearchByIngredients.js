519f12e0dc9e4a3c5a2c9a7797e93e8da702449e
Brenda Cheptoo <bcheptoo85@gmail.com>
2024-05-07 21:02:23 +0300

    Function to handle input change

--- src/SearchByIngredients.js
+++ src/SearchByIngredients.js
@@ -2,7 +2,12 @@ import React, { useState } from "react";
 
 function SearchByIngredients=() => {
     const[ ingredients, setIngredients]=useState("");
-    const [searchResults, setSearchResults]= useState([])
+    const [searchResults, setSearchResults]= useState([]);
 }
+
+
+    const handleInputCange = (event) => {
+        setIngredients(event.target.value)
+    }
 
 export default SearchByIngredients;
