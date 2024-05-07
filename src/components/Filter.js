c727171ff039c327f42bd27607ce89db0224d066
Favor Vincent <vfavorm77@gmail.com>
2024-05-07 21:59:15 +0300

    introduce a dropdown with all the categories

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,31 +1,29 @@
-import { useEffect, useState } from "react";
+import React from "react";
 
 function CocktailList() {
-  const [cocktails, setCocktails] = useState([]);
+  
 
-  useEffect(() => {
-    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
-      .then(res => res.json())
-      .then(data => {setCocktails(data.drinks);
-      })
-      .catch(error => console.log(`Error fetching data:${error}`));
-  }, []);
-
- console.log(cocktails);
-
-const categories = [
-    "Ordinary Drink",
-    "Cocktail",
-    "Milk / Float / Shake",
-    "Other/Unknown",
-    "Cocoa",
-    "Shot",
-    "Coffee / Tea",
-    "Homemade Liqueur",
-    "Punch / Party-Drink",
-    "Beer",
-    "Soft Drink / Soda"
-]
+  return(
+    <select
+    name="filter"
+    value={category}
+    onChange={(e) => onCategoryChange(e.target.value)}
+  >
+    <option value="All">Filter by category</option>
+    <option value="Ordinary Drink">Ordinary Drink</option>
+    <option value="Cocktail">Cocktail</option>
+    <option value="Milk / Float / Shake">Milk / Float / Shake</option>
+    <option value="Other/Unknown">Other/Unknown</option>
+    <option value="Cocoa">Cocoa</option>
+    <option value="Shot">Shot</option>
+    <option value="Coffee / Tea">Coffee / Tea</option>
+    <option value="Homemade Liqueur">Homemade Liqueur</option>
+    <option value="Punch / Party Drink">Punch / Party Drink</option>
+    <option value="Beer">Beer</option>
+    <option value="Soft Drink / Soda">Soft Drink / Soda</option>
+  
+  </select>
+  )
 }
 
 export default CocktailList;
