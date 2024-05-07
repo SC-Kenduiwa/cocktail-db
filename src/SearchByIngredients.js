df04e1e3a0e72f7c6dc8a5e72d0f7248498b6428
Brenda Cheptoo <bcheptoo85@gmail.com>
2024-05-07 22:02:12 +0300

    Search button added

--- src/SearchByIngredients.js
+++ src/SearchByIngredients.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const SearchByIngredients = () => {
@@ -8,36 +8,37 @@ const SearchByIngredients = () => {
   useEffect(() => {
     const fetchCocktails = async () => {
       try {
-        const response = await axios.get(
-          `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredients}`
-        );
-        setSearchResults(response.data.drinks || []);
+        if (ingredients.trim() !== "") {
+          const response = await axios.get(
+            `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredients}`
+          );
+          setSearchResults(response.data.drinks || []);
+        } else {
+          setSearchResults([]);
+        }
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
 
-    if (ingredients !== "") {
-      fetchCocktails();
-    }
+    fetchCocktails();
   }, [ingredients]);
 
   const handleInputChange = (event) => {
     setIngredients(event.target.value);
   };
 
-
   return (
     <div>
       <h1>Search Cocktails by Ingredients</h1>
-      <form onSubmit={handleSubmit}>
+      <form>
         <input
           type="text"
-          placeholder="Enter ingredients"
+          placeholder="Enter ingredients separated by commas..."
           value={ingredients}
           onChange={handleInputChange}
         />
-        <button type="submit">Search</button>
+        <button type="Submit">Search</button>
       </form>
       <h2>Search Results</h2>
       <div className="cocktail-list">
@@ -55,7 +56,5 @@ const SearchByIngredients = () => {
     </div>
   );
 };
-
-     
 
 export default SearchByIngredients;
