69c850abdef52ea7e46fbd26127e1ae10cf65d08
Shanice Chebet Kenduiwa <kenduiwashanice63@gmail.com>
2024-05-07 23:26:43 +0300

    filter component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import SearchByIngredients from './SearchByIngredients';
-
+import CocktailFilter from './CocktailFilter'; 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to the Cocktail Database</h1>
       </header>
+      <main>
+        <CocktailFilter />
+      </main>
     </div>
   );
 }
-
-export default App;
