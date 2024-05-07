8d705786e47451f9d68aa75f7951d87e246f0e1e
Brenda Cheptoo <bcheptoo85@gmail.com>
2024-05-07 20:58:41 +0300

    Defining SearchByIngredients functional component and initializing state of ingredient

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SearchByIngredients from './SearchByIngredients';
 function App() {
   return (
     <div className="App">
+      <SearchByIngredients />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/SearchByIngredients.js
+++ src/SearchByIngredients.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 
-function SearchByIngredients(){
-    const[ ingredients, setIngredients]=useState("")
+function SearchByIngredients=() => {
+    const[ ingredients, setIngredients]=useState("");
+    const [searchResults, setSearchResults]= useState([])
 }
 
 export default SearchByIngredients;
