977443772950c39b6d12cb9a13c83e0c9f7cb54f
Favor Vincent <vfavorm77@gmail.com>
2024-05-07 22:15:32 +0300

    introduce a state to store the selected category

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CategoryFilter() {
-  
+  const [chosenCategory, setChosenCategory] =useState([]);
+
+  function onCategoryChange (category){
+  }
 
   return(
  <div>  
