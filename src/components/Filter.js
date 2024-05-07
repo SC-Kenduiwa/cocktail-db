290b64ecdb483d7e04e9cbb9118ecac5d636c3ad
Favor Vincent <vfavorm77@gmail.com>
2024-05-07 21:39:18 +0300

    Set the categories

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -13,6 +13,19 @@ function CocktailList() {
 
  console.log(cocktails);
 
+const categories = [
+    "Ordinary Drink",
+    "Cocktail",
+    "Milk / Float / Shake",
+    "Other/Unknown",
+    "Cocoa",
+    "Shot",
+    "Coffee / Tea",
+    "Homemade Liqueur",
+    "Punch / Party-Drink",
+    "Beer",
+    "Soft Drink / Soda"
+]
 }
 
 export default CocktailList;
