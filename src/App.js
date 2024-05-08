7f42b65524dd37b0129f4a3a1ab8295ecfab05a3
SC-Kenduiwa <kenduiwashanice64@gmail.com>
2024-05-08 19:50:26 +0300

    complete work

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React from 'react';
-import SearchByIngredients from './SearchByIngredients';
-import CocktailFilter from './CocktailFilter'; 
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import FilterAlcohol from './FilterAlcohol'; 
+import Home from './Home'; 
 
+// App component
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to the Cocktail Database</h1>
-      </header>
-      <main>
-        <CocktailFilter />
-      </main>
-    </div>
+    <Router>
+      <div className="App">
+        <nav style={{ textAlign: 'center', margin: '20px 0' }}>
+          <ul style={{ listStyleType: 'none', padding: 0 }}>
+            <li style={{ display: 'inline-block', marginRight: '20px' }}>
+              <Link to="/" style={{ textDecoration: 'none', color: 'black' }}>Home</Link>
+            </li>
+            <li style={{ display: 'inline-block' }}>
+              <Link to="/FilterAlcohol" style={{ textDecoration: 'none', color: 'black' }}>Filter Alcohol</Link>
+            </li>
+            
+          </ul>
+        </nav>
+        <header className="App-header">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/FilterAlcohol" element={<FilterAlcohol />} />
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
+
+export default App;
