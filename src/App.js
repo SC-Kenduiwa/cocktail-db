bad2d968c8aeabd9968b6b9afb8b54511918b50b
Lorraine Opiyo <achienglorraine8@gmail.com>
2024-05-08 21:15:29 +0300

    merge changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import SearchByIngredients from './SearchByIngredients';
-import FilterAlcohol from './FilterAlcohol'
+//import FilterAlcohol from './FilterAlcohol'
 import './App.css';
-import React from "react";
 import CategoryFilter from "./Filter"
 import ListCocktails from './ListCocktails';
 
 function App() {
   return (
     <div className="App">
-      <main>
-        <FilterAlcohol />
+      <main> 
         <SearchByIngredients/>
           <CategoryFilter/>
           <ListCocktails/>
