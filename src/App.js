abdca6e3ac9439c48422265a99c7b24aacc13908
Lorraine Opiyo <achienglorraine8@gmail.com>
2024-05-08 19:47:23 +0300

    search by name

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import FilterAlcohol from './FilterAlcohol'; 
-import Home from './Home'; 
+import SearchByIngredients from './SearchByIngredients';
+import FilterAlcohol from './FilterAlcohol'
+import './App.css';
+import React from "react";
+import CategoryFilter from "./Filter"
+import ListCocktails from './ListCocktails';
 
-// App component
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <nav style={{ textAlign: 'center', margin: '20px 0' }}>
-          <ul style={{ listStyleType: 'none', padding: 0 }}>
-            <li style={{ display: 'inline-block', marginRight: '20px' }}>
-              <Link to="/" style={{ textDecoration: 'none', color: 'black' }}>Home</Link>
-            </li>
-            <li style={{ display: 'inline-block' }}>
-              <Link to="/FilterAlcohol" style={{ textDecoration: 'none', color: 'black' }}>Filter Alcohol</Link>
-            </li>
-            
-          </ul>
-        </nav>
-        <header className="App-header">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/FilterAlcohol" element={<FilterAlcohol />} />
-          </Routes>
-        </header>
-      </div>
-    </Router>
+    <div className="App">
+      <main>
+        <FilterAlcohol />
+        <SearchByIngredients/>
+          <CategoryFilter/>
+          <ListCocktails/>
+      </main>
+    </div>
   );
 }
 
