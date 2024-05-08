4039b304e2f2b813de0bff3cfb5fdc132051f181
Brenda Cheptoo <bcheptoo85@gmail.com>
2024-05-08 08:39:54 +0300

    Styling of the headings

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import SearchByIngredients from './SearchByIngredients';
 
@@ -7,20 +6,6 @@ function App() {
   return (
     <div className="App">
       <SearchByIngredients />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
--- src/SearchByIngredients.js
+++ src/SearchByIngredients.js
@@ -1,44 +1,45 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 const SearchByIngredients = () => {
   const [ingredients, setIngredients] = useState("");
   const [searchResults, setSearchResults] = useState([]);
 
-  useEffect(() => {
-    const fetchCocktails = async () => {
-      try {
-        if (ingredients.trim() !== "") {
-          const response = await axios.get(
-            `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredients}`
-          );
-          setSearchResults(response.data.drinks || []);
-        } else {
-          setSearchResults([]);
-        }
-      } catch (error) {
-        console.error("Error fetching data:", error);
+  const fetchCocktails = async () => {
+    try {
+      if (ingredients.trim() !== "") {
+        const response = await axios.get(
+          `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredients}`
+        );
+        setSearchResults(response.data.drinks || []);
+      } else {
+        setSearchResults([]);
       }
-    };
-
-    fetchCocktails();
-  }, [ingredients]);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
   const handleInputChange = (event) => {
     setIngredients(event.target.value);
   };
 
+  const handleSearch = async (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+    fetchCocktails(); // Call fetchCocktails when the search button is clicked
+  };
+
   return (
     <div>
       <h1>Search Cocktails by Ingredients</h1>
-      <form>
+      <form onSubmit={handleSearch}>
         <input
           type="text"
-          placeholder="Enter ingredients separated by commas..."
+          placeholder="Enter ingredients"
           value={ingredients}
           onChange={handleInputChange}
         />
-        <button type="Submit">Search</button>
+        <button type="submit">Search</button>
       </form>
       <h2>Search Results</h2>
       <div className="cocktail-list">
