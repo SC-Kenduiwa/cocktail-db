82387e46a2e6ff50502564550087fe8f20707f3e
Lorraine Opiyo <achienglorraine8@gmail.com>
2024-05-08 21:53:09 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import SearchByIngredients from './SearchByIngredients';
-//import FilterAlcohol from './FilterAlcohol'
+import FilterAlcohol from './FilterAlcohol'
 import './App.css';
 import CategoryFilter from "./Filter"
 import ListCocktails from './ListCocktails';
+import SearchByName from './SearchByName';
 
 function App() {
   return (
     <div className="App">
       <main> 
+        <FilterAlcohol/>
         <SearchByIngredients/>
           <CategoryFilter/>
           <ListCocktails/>
+          <SearchByName/>
       </main>
     </div>
   );
