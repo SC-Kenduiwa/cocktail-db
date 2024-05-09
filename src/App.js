eebfc1ea6b6e07233e2610051db59e1d84cd6d1f
Favor Vincent <vfavorm77@gmail.com>
2024-05-09 09:37:53 +0300

    made changes to app.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import FilterAlcohol from './FilterAlcohol'; 
 import Home from './Home'; 
 import CategoryFilter from './Filter';
-import "./Home.css";
+import SearchByIngredients from './SearchByIngredients'
 
 // App component
 function App() {
   return (
     <Router>
       <div className="App">
-        <nav style={{ textAlign: 'center', margin: '20px 0' }} >
+        <nav style={{ textAlign: 'center', margin: '20px 0' }}>
           <ul style={{ listStyleType: 'none', padding: 0 }}>
             <li style={{ display: 'inline-block', marginRight: '20px' }} className="navlinks">
               <Link to="/" style={{ textDecoration: 'none', color: 'black' }}>Home</Link>
@@ -21,7 +21,13 @@ function App() {
             <li style={{ display: 'inline-block' }} className="navlinks">
               <Link to="/CategoryFilter" style={{ textDecoration: 'none', color: 'black', marginRight: "20px" }}>Explore by Category</Link>
             </li>
+            <li style={{ display: 'inline-block' }} className="navlinks">
+              <Link to="/SearchByIngredients" style={{ textDecoration: 'none', color: 'black', marginRight: "20px" }}>Search By Ingredients</Link>
+              </li>
             
+            <li style={{ display: 'inline-block' }} className="navlinks">
+              <Link to="/SearchByName" style={{ textDecoration: 'none', color: 'black' }}>Search By Name</Link>
+            </li>
           </ul>
         </nav>
         <header className="App-header">
@@ -29,6 +35,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/FilterAlcohol" element={<FilterAlcohol />} />
             <Route path="/CategoryFilter" element={<CategoryFilter/>} />
+            <Route path="/SearchByIngredients" element={<SearchByIngredients />} />
           </Routes>
         </header>
       </div>
